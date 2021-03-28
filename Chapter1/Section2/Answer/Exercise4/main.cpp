118a04ff7b934497e15a55aa2f20764c11e96ab9
scyhssm <347966819@qq.com>
2021-03-28 20:46:37 +0800

    c1 s2 answer

---
+++ Chapter1/Section2/Answer/Exercise4/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int x = 0, y = 0;
+    std::cin >> x >> y;
+    std::cout << "The product of " << x << " and " << y << " is " << x * y << std::endl;
+    return 0;
+}
---
+++ Chapter1/Section2/Answer/Exercise5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int x = 0, y = 0;
+    std::cin >> x >> y;
+    std::cout << "The product of ";
+    std::cout << x;
+    std::cout << " and ";
+    std::cout << y;
+    std::cout << " is ";
+    std::cout << x * y;
+    std::cout << std::endl;
+    return 0;
+}
