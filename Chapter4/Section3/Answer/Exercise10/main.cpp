7713cdb4a1f9031f62427d3c6b1d78fc9e56fc71
scyhssm <347966819@qq.com>
2021-04-13 21:00:29 +0800

    c4 s3 answer

---
+++ Chapter4/Section3/Answer/Exercise10/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    int num;
+    while (std::cin >> num && num != 42) {}
+    return 0;
+}
---
+++ Chapter4/Section3/Answer/Exercise11/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int a, b, c, d;
+    // input a b c d
+    if (a > b && b > c && c > d) {}
+    return 0;
+}
